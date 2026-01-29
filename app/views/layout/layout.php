71446704f0c51043b37dd52987dafb3955496aa8
RoiVegetto <benjamincorroyez@hotmail.fr>
2026-01-29 16:42:12 +0100

    Save pour passer du PC pro au PC perso
    CSS amélioré car cassé ( position relative sur le header qui impacte tout le site )

--- app/views/layout/layout.php
+++ app/views/layout/layout.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($title) ?></title>
-    <link rel="stylesheet" href="/tomtroc/public/css/style.css">
+    <link rel="stylesheet" href="/tomtroc/public/css/assets/style.css">
 </head>
 <body>
     <?php require __DIR__ . '/header.php'; ?>
